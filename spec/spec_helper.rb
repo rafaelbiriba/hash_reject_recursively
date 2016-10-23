88d1a64fc7da8db18207e5dd3a22e8205982851b
Rafael Biriba <biribarj@gmail.com>
2016-10-23 15:49:42 -0200

    Fix Coveralls require issue

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "hash_reject_recursively"
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "hash_reject_recursively"
